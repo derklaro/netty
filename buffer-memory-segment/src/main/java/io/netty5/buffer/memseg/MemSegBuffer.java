dfe0828af0fb35cd6ed622621dd7d86e4f124b6d
Pasqual Koschmieder <git@derklaro.dev>
2025-05-14 17:40:40 +0200

    remove assertion that shouldn't be there

--- buffer-memory-segment/src/main/java/io/netty5/buffer/memseg/MemSegBuffer.java
+++ buffer-memory-segment/src/main/java/io/netty5/buffer/memseg/MemSegBuffer.java
@@ -201,7 +201,6 @@ class MemSegBuffer extends AdaptableBuffer<MemSegBuffer>
 
     @Override
     public int readableArrayLength() {
-        assertHasArray(seg);
         return woff - roff;
     }
 
@@ -237,7 +236,6 @@ class MemSegBuffer extends AdaptableBuffer<MemSegBuffer>
 
     @Override
     public int writableArrayLength() {
-        assertHasArray(wseg);
         return writableBytes();
     }
 
